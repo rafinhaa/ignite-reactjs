7277178ef10c0dad81c1814d8873772787a6dbe0
rafinhaa <rafinhaa@me.com>
2021-09-06 19:20:27 -0300

    - Terminado a aula 02 - Conceitos importantes - 05 - Fast Refresh no Webpack

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -17,18 +18,29 @@ module.exports = {
     devServer:{
         static: path.resolve(__dirname, 'public'), //Pasta onde fica o arquivo HTML (conteúdo estatico)
         port: 3000,
+        hot: true,
     },
     plugins: [
+        new ReactRefreshWebpackPlugin({
+            overlay: false,
+        }), //Se estiver em Desenvolvimento, ativa o plugin ReactRefreshWebpackPlugin se não, retonar FALSE
         new HtmlWebpackPlugin({ //Plugin para gerar o arquivo index.html
             template: path.resolve(__dirname,'public','index.html'), //Template que será utilizado para gerar o arquivo index.html
         })
-    ],
+    ].filter(Boolean), //Caso algum plugin seja FALSE, ele será removido do array
     module: {
         rules: [
             {
                 test: /\.jsx$/, //Expressão regular para encontrar arquivos js ou jsx
                 exclude: /node_modules/, //Excluir arquivos de node_modules
-                use: 'babel-loader', //Utilizar o babel-loader para transpilar o arquivo
+                use: {
+                    loader: 'babel-loader', //Babel-loader é responsável por converter o código jsx para js
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel') //Se estiver em Desenvolvimento, ativa o plugin ReactRefreshWebpackPlugin se não, retonar FALSE
+                        ].filter(Boolean), //Caso algum plugin seja FALSE, ele será removido do array
+                    }
+                },
             },
             {
                 test: /\.scss$/, //Expressão regular para encontrar arquivos scss
